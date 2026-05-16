e01926d358f3ba8bfe60a4fa93ba522ea1472f5d
mtjf development <dev@mtjfoundation.org>
2026-05-16 17:53:58 +0500

    updates

--- src/components/home/CurtainOverlay.tsx
+++ src/components/home/CurtainOverlay.tsx
@@ -16,7 +16,7 @@ type Props = {
 // Once per full page load / tab session; skipped on client navigations back to home.
 let hasShownCurtainOverlayInSession = false;
 
-const DEFAULT_HOLD_MS = 30;
+const DEFAULT_HOLD_MS = 250;
 const DEFAULT_SLIDE_MS = 380;
 
 export default function CurtainOverlay({
